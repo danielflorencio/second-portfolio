98bf687086505c36eb6c31996677716a6a23d2dd
danielflorencio <danielflorencio20@gmail.com>
2023-08-27 00:28:02 -0300

    Testing github actions workflow.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <title>Daniel Portfolio</title>
     </head>
     
+    
     <main className={styles.main}>
       <NavbarContainer/>
       <HeroSectionOne/>
