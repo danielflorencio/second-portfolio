7ab8763e7d6bc669447e3d97dec0adc5d912527e
danielflorencio <danielflorencio20@gmail.com>
2023-08-30 20:06:18 -0300

    .

---
+++ src/sectionLayouts/GitInfo/Index.tsx
@@ -0,0 +1,98 @@
+'use client'
+import { useQuery, gql } from '@apollo/client';
+import { useEffect, useState } from 'react';
+import styles from './styles.module.css'
+
+const GET_CONTRIBUTION_DAYS = gql`
+query {
+    viewer {
+        repositories(isFork: false, privacy: PUBLIC) {
+            totalCount
+        }
+        contributionsCollection {
+            contributionCalendar {
+                totalContributions
+                weeks {
+                    contributionDays {
+                        contributionCount
+                    }
+                }
+            }
+        }
+    }
+}
+`
+
+// const GET_SCHEMA = gql`
+// query {
+//     __schema {
+//       types {
+//         name
+//         kind
+//         description
+//         fields {
+//           name
+//         }
+//       }
+//     }
+//   }  
+// `
+
+export default function GitInfo(){
+
+    const { data: contributionData } = useQuery(GET_CONTRIBUTION_DAYS);
+
+    // const graphqlSchema = useQuery(GET_SCHEMA);
+
+    // console.log('GraphQL Schema: ', graphqlSchema)
+
+    const [commits, setCommits] = useState();
+    const [streak, setStreak] = useState<number>();
+    const [repos, setRepos] = useState();
+
+    useEffect(() => {
+        let streaks: number[] = [0];
+        if(contributionData){
+            console.log('ContributionData: ', contributionData)
+            for(let i = 0; i < contributionData.viewer.contributionsCollection.contributionCalendar.weeks.length; i++){
+                for(let n = 0; n < contributionData.viewer.contributionsCollection.contributionCalendar.weeks[i].contributionDays.length; n++){
+                    if(contributionData.viewer.contributionsCollection.contributionCalendar.weeks[i].contributionDays[n].contributionCount > 0){
+                        streaks[streaks.length - 1] = streaks[streaks.length - 1] + 1
+                    } else{
+                        if(streaks[streaks.length - 1] !== 0){
+                            streaks.push(0)
+                        }
+                    }
+                }
+            }
+            const highestStreak = Math.max(...streaks)
+            setStreak(highestStreak)
+            setCommits(contributionData.viewer.contributionsCollection.contributionCalendar.totalContributions)
+            setRepos(contributionData.viewer.repositories.totalCount)
+        }
+    }, [contributionData])
+
+    return(
+        <section className={styles.section}>
+            <h1 className={styles.title}>The best developers are always improving themselves.</h1>
+            <h2 className={styles.subtitle}>See my Github stats as my coding skills grow sharper every single day.</h2>
+            <div className={styles.infoBlocks}>
+                <div className={styles.infoBlock}>
+                    <p className={styles.infoBlockNumber}>{commits}</p>
+                    <h3 className={styles.infoBlockTitle}>Commits in the last year</h3>
+                </div>
+
+                <div className={styles.infoBlock}>
+                    <p className={styles.infoBlockNumber}>{streak}</p>
+                    <h3 className={styles.infoBlockTitle}>Highest streak</h3>
+                </div>
+
+                <div className={styles.infoBlock}>
+                    <p className={styles.infoBlockNumber}>{repos}</p>
+                    <h3 className={styles.infoBlockTitle}>Repos contributed</h3>
+                </div>
+
+            </div>
+        </section>
+    )
+}
