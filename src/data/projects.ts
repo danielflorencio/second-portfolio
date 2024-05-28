c6b52d758761b361ce7ae4c3f675888da7a011c9
Daniel Florencio <danielflorencio20@gmail.com>
2024-05-27 23:07:47 -0300

    switched the chatbot project for the smart catcher project.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,28 +1,54 @@
-export const projects = [
-    { imgSrc: '/calon-website.png', 
-    title: 'Calon - Digital Marketing Agency Landing Page', 
-    descriptionParagraphs: 
-    ['Created a High-speed and conversion-focused landing page for a Brazilian digital marketing agency.', 
-     'I also crafted the copywriting for the page and ensured its deployment to production, making it readily accessible within a cloud environment.'], 
-    skills: ['HTML', 'CSS Modules', 'Next.Js', 'Bootstrap', 'TypeScript'], 
-    liveLink: 'https://calon-website.pages.dev/',
-    repoLink: 'https://calon-website.pages.dev/',
-    direction: 'direct',
-    customHeight: '400',
-    customWidth: '330'
-    
+export type TProject = {
+    customHeight: number, 
+    customWidth: number,
+    imgSrc: string,
+    title: string, 
+    descriptionParagraphs: string[],
+    liveLink?: string,
+    repoLink: string,
+    skills: string[],
+    direction: 'direct' | 'reverse'
+}
+
+export const projects: TProject[] = [
+    { 
+        imgSrc: '/calon-website.png', 
+        title: 'Calon - Digital Marketing Agency Landing Page', 
+        descriptionParagraphs: 
+        ['Created a High-speed and conversion-focused landing page for a Brazilian digital marketing agency.', 
+        'I also crafted the copywriting for the page and ensured its deployment to production, making it readily accessible within a cloud environment.'], 
+        skills: ['HTML', 'CSS Modules', 'Next.Js', 'TypeScript'], 
+        liveLink: 'https://calon-website.pages.dev/',
+        repoLink: 'https://calon-website.pages.dev/',
+        direction: 'direct',
+        customHeight: 400,
+        customWidth: 330
     },
-    
-    { imgSrc: '/chatbot-app.png', 
-    title: 'Chatbot micro SaaS Project', 
-    descriptionParagraphs: 
-    ["This is a full-stack application I'm currently building on my own to automate conversations, increase sales, and reduce costs with attendants of companies in multiple industries.", 
-    'It already has a complete JWT Authentication system, a complete messaging functionality which persists data in the database, with more features and improvements coming in the future.'],
-    skills: ['React', 'TypeScript', 'Material UI', 'Node.Js', 'Express.Js', 'MongoDB', 'Docker', 'Cypress', 'Jest', 'React Testing Library'], 
-    liveLink: 'https://github.com/danielflorencio/chatbot-monorepo',
-    repoLink: 'https://github.com/danielflorencio/chatbot-monorepo',
-    direction: 'reverse',
-    customHeight: '200',
-    customWidth: '400'
-},
+    {
+        imgSrc: '/smartcatcher.png',
+        title: 'Smart Catcher - A.I Surveillance Startup',
+        descriptionParagraphs: [
+            `This is a SaaS project for a UK based start up i worked for some time. The idea of the product was really nice, and I was able to contribute in every aspect of it's development, from the Golang Backend, to the AWS infrastructure and the React Frontend.`,
+            `Unfortunately, the investors decided to shut down the development of the project, and I am not allowed to share neither the code nor the project's appearence, but I can show the Landing page :)`
+        ],
+        skills: ['Next.js', 'TypeScript', 'Golang', 'AWS', 'Tailwind', 'Material UI'],
+        liveLink: 'https://smartcatcher.vercel.app/',
+        repoLink: 'https://github.com/danielflorencio/scatcher-website',
+        direction: 'reverse',
+        customHeight: 220,
+        customWidth: 400
+    }
+   
+//     { imgSrc: '/chatbot-app.png', 
+//     title: 'Chatbot micro SaaS Project', 
+//     descriptionParagraphs: 
+//     ["This is a full-stack application I'm currently building on my own to automate conversations, increase sales, and reduce costs with attendants of companies in multiple industries.", 
+//     'It already has a complete JWT Authentication system, a complete messaging functionality which persists data in the database, with more features and improvements coming in the future.'],
+//     skills: ['React', 'TypeScript', 'Material UI', 'Node.Js', 'Express.Js', 'MongoDB', 'Docker', 'Cypress', 'Jest', 'React Testing Library'], 
+//     liveLink: 'https://github.com/danielflorencio/chatbot-monorepo',
+//     repoLink: 'https://github.com/danielflorencio/chatbot-monorepo',
+//     direction: 'reverse',
+//     customHeight: 200,
+//     customWidth: 400
+// },
 ]
--- src/sectionLayouts/Projects/Index.tsx
+++ src/sectionLayouts/Projects/Index.tsx
@@ -1,55 +1,26 @@
 import {FiExternalLink} from 'react-icons/fi'
-import { projects } from '@/data/projects'
+import { TProject, projects } from '@/data/projects'
 import Image from 'next/image'
 import styles from './styles.module.css'
+
 export default function Projects(){
     return(
         <section id='projects' className={styles.section}>
             <h2 className={styles.sectionTitle}>Take a look at some of the projects I've made.</h2>
-            {/* <h3 className={styles.sectionSubtitle}></h3> */}
             <div className={styles.container}>
-                <ProjectOne project={projects[0]}/>
-                <ProjectTwo project={projects[1]}/>
+                <PortfolioProject project={projects[0]}/>
+                <PortfolioProject project={projects[1]}/>
             </div>
         </section>
     )
 }
 
-function ProjectOne({project}: {project: any}){
-    return(
-        <div className={styles.projectBlock}>
-            <div className={styles.projectImgContainer}>
-                <Image alt='' height={`${project.customHeight}`} width={`${project.customWidth}`} src={project.imgSrc} className={styles.projectImg}/>
-            </div>
-            <div className={styles.projectDescriptionContainer}>
-                <div>
-                    <div className={styles.titleAndIconContainer}>
-                        <h3 className="">{project.title}</h3>
-                        <a href={project.liveLink} className="hover:text-sky-400 transition-colors duration-150" target="_blank" rel="noopener noreferrer">
-                            <FiExternalLink/>
-                        </a>
-                    </div>
-                    <div className={styles.descriptionParagraphs}>
-                    {project.descriptionParagraphs.map((paragraph: any, pIndex: number) =>(
-                        <p key={pIndex} className={styles.descriptionParagraph}>{paragraph}<br/><br/></p>
-                    ))}
-                    </div>
-                </div>
-                <div className={styles.skillList}>
-                    {project.skills.map((skill: any, skillIndex: number) => (
-                        <div key={skillIndex} className={styles.skillItem}>{skill}</div>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
-}
 
-function ProjectTwo({project}: {project: any}){
+const PortfolioProject = ({project}: {project: TProject}) => {
     return(
         <div className={styles.projectBlock}>
             <div className={styles.projectImgContainer}>
-                <Image alt='' height={`${project.customHeight}`} width={`${project.customWidth}`} src={project.imgSrc} className={styles.projectImg}/>
+                <Image alt='' height={project.customHeight} width={project.customWidth} src={project.imgSrc} className={styles.projectImg}/>
             </div>
             <div className={styles.projectDescriptionContainer}>
                 <div>
@@ -63,7 +34,8 @@ function ProjectTwo({project}: {project: any}){
                     {project.descriptionParagraphs.map((paragraph: any, pIndex: number) =>(
                         <p key={pIndex} className={styles.descriptionParagraph}>{paragraph}<br/><br/></p>
                     ))}
-                    </div>                </div>
+                    </div>
+                </div>
                 <div className={styles.skillList}>
                     {project.skills.map((skill: any, skillIndex: number) => (
                         <div key={skillIndex} className={styles.skillItem}>{skill}</div>
